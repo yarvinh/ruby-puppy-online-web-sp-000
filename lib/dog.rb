bc73f564a0f044574bfb2e7cb9d1913aca0831c0
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-15 23:54:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+
+class Dog
+    @@all = []
+    @@all_dog = []
+    @@save = []
+  attr_accessor :name, :save
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@all_dog << name
+    save
+  end
+  def self.all
+      @@all
+  end
+  def self.clear_all
+     @@all_dog.clear
+     @@all.clear
+  end
+ def self.print_all
+   puts @@all_dog
+end
+def save
+@@all << self
+@@all.uniq!
+end
+
+
+end
